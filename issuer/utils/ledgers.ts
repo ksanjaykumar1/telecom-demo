75e3233618f14c9a9318787fdea5eedcc66a3dea
Sanjay <ksanjayk@protonmail.com>
2023-05-20 19:30:07 +0530

    modified for 0.4

--- issuer/utils/ledgers.ts
+++ issuer/utils/ledgers.ts
@@ -1,10 +1,9 @@
 import { pool_transactions_bcovrin_test_genesis } from './txns';
+import type { IndyVdrPoolConfig } from '@aries-framework/indy-vdr';
 
-export const ledgers = [
-  {
-    id: `bcovrin-test-net`,
-    isProduction: false,
-    indyNamespace: 'bcovrin:test',
-    genesisTransactions: pool_transactions_bcovrin_test_genesis,
-  },
-];
+export const ledgers = {
+  isProduction: false,
+  indyNamespace: 'bcovrin:test',
+  connectOnStartup: true,
+  genesisTransactions: pool_transactions_bcovrin_test_genesis,
+};

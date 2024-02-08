edb40201ea6a4514a0fdca024c416852feb1d00b
Sanjay <ksanjayk@protonmail.com>
2024-02-08 13:14:36 +0530

    comments added

--- agent/src/agent/did.ts
+++ agent/src/agent/did.ts
@@ -1,8 +1,12 @@
 import { KeyType, TypedArrayEncoder } from '@aries-framework/core';
 import { DID_NAMESPACE, DID, SEED } from '../constants/constant';
 import { CustomAgent } from './agent';
-// import { log, underscore, yellow } from '../utils';
 
+/**
+ * Import an existing did that was created outside.
+ * This will create a `DidRecord` for the did
+ * and will allow the did to be used in other parts of the agent.
+ */
 export const createAndRegisterIndy = async (issuer: CustomAgent) => {
   const seed = TypedArrayEncoder.fromString(SEED);
   const indyDID = `${DID_NAMESPACE}${DID}`;

32af272cf2e6a22eb5d74b660e11040cdeb3f802
Sanjay <ksanjayk@protonmail.com>
2024-02-08 13:10:35 +0530

    modules required for agent

---
+++ agent/src/agent/agentModule.ts
@@ -0,0 +1,62 @@
+import {
+  AutoAcceptCredential,
+  AutoAcceptProof,
+  ConnectionsModule,
+  CredentialsModule,
+  DidsModule,
+  ProofsModule,
+  V2CredentialProtocol,
+  V2ProofProtocol,
+} from '@aries-framework/core';
+import { AskarModule } from '@aries-framework/askar';
+import { ariesAskar } from '@hyperledger/aries-askar-nodejs';
+import { AnonCredsRsModule } from '@aries-framework/anoncreds-rs';
+import { anoncreds } from '@hyperledger/anoncreds-nodejs';
+import {
+  AnonCredsCredentialFormatService,
+  AnonCredsModule,
+  AnonCredsProofFormatService,
+} from '@aries-framework/anoncreds';
+import {
+  IndyVdrAnonCredsRegistry,
+  IndyVdrIndyDidRegistrar,
+  IndyVdrIndyDidResolver,
+  IndyVdrModule,
+} from '@aries-framework/indy-vdr';
+import { indyVdr } from '@hyperledger/indy-vdr-nodejs';
+import { bcovrinTestNetwork } from '../constants/networks';
+
+export const agentModules = {
+  askar: new AskarModule({ ariesAskar }),
+  anoncredsRs: new AnonCredsRsModule({
+    anoncreds,
+  }),
+  anoncreds: new AnonCredsModule({
+    registries: [new IndyVdrAnonCredsRegistry()],
+  }),
+  indVdr: new IndyVdrModule({
+    indyVdr,
+    networks: [bcovrinTestNetwork],
+  }),
+  connections: new ConnectionsModule({ autoAcceptConnections: true }),
+  dids: new DidsModule({
+    registrars: [new IndyVdrIndyDidRegistrar()],
+    resolvers: [new IndyVdrIndyDidResolver()],
+  }),
+  credentials: new CredentialsModule({
+    autoAcceptCredentials: AutoAcceptCredential.Always,
+    credentialProtocols: [
+      new V2CredentialProtocol({
+        credentialFormats: [new AnonCredsCredentialFormatService()],
+      }),
+    ],
+  }),
+  proofs: new ProofsModule({
+    autoAcceptProofs: AutoAcceptProof.Always,
+    proofProtocols: [
+      new V2ProofProtocol({
+        proofFormats: [new AnonCredsProofFormatService()],
+      }),
+    ],
+  }),
+};

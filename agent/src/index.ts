52e5f74b6584c8b3680c69c26393488249c156cc
Sanjay <ksanjayk@protonmail.com>
2024-02-12 11:35:43 +0530

    logger and morgan added

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -3,24 +3,37 @@ import dotenv from 'dotenv';
 import { agent } from './agent/agent';
 import { AGENT_TYPE } from './constants/constant';
 import { createAndRegisterIndy } from './agent/did';
-import morganMiddleware from './util/morgan';
+import morganMiddleware from './utils/morgan';
+import Logger from './utils/loggers';
+import connections from './routes/connections';
+
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use(morganMiddleware);
+app.post('/', (req: Request, res: Response) => {
+  Logger.debug(req.body);
+  res.send('Express + TypeScript Server');
+});
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
+app.use('/connections', connections);
 app.listen(port, async () => {
-  // Agent initialization
-  await agent.initialize();
-  if (AGENT_TYPE === 'issuer') {
-    await createAndRegisterIndy(agent);
+  try {
+    await agent.initialize();
+    if (AGENT_TYPE === 'issuer') {
+      await createAndRegisterIndy(agent);
+    }
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  } catch (error) {
+    Logger.error(error);
+    // process.exit(1);
   }
-
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  // Agent initialization
 });

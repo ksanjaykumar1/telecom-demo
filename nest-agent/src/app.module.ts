af695fd07fe783451302ac1d6933c25d250302ee
Sanjay <ksanjayk@protonmail.com>
2024-02-12 17:03:40 +0530

    initla agent setup

--- nest-agent/src/app.module.ts
+++ nest-agent/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AgentModule } from './agent/agent.module';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './agent/config/app.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [appConfig],
+    }),
+    AgentModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

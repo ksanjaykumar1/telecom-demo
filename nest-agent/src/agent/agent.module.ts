df234da5e76e4e25852ca741629dc1a34fc34b96
Sanjay <ksanjayk@protonmail.com>
2024-02-14 19:06:32 +0530

    agent setup and importing did

--- nest-agent/src/agent/agent.module.ts
+++ nest-agent/src/agent/agent.module.ts
@@ -1,4 +1,69 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
+import { AGENT_TOKEN } from 'src/constants/constant';
+import { ConfigService } from '@nestjs/config';
+import {
+  Agent,
+  ConsoleLogger,
+  HttpOutboundTransport,
+  InitConfig,
+  LogLevel,
+  WsOutboundTransport,
+} from '@aries-framework/core';
+import { HttpInboundTransport, agentDependencies } from '@aries-framework/node';
+import { agentModuleFactory, createAndRegisterIndy } from './agentUtils';
 
-@Module({})
-export class AgentModule {}
+const agentFactory = {
+  provide: AGENT_TOKEN,
+  useFactory: async (configService: ConfigService, agentModules: any) => {
+    const {
+      label,
+      agent_port,
+      did,
+      seed,
+      wallet_key,
+      wallet_id,
+      endpoint,
+      did_namespace,
+    } = configService.get('agent');
+    const applicationEnv = configService.get('environment');
+    const agentEndpoints = applicationEnv === 'development' ? [] : [endpoint];
+    const config: InitConfig = {
+      label: label,
+      endpoints: agentEndpoints,
+      walletConfig: {
+        id: wallet_id,
+        key: wallet_key,
+      },
+      logger: new ConsoleLogger(
+        applicationEnv === 'development' ? LogLevel.debug : LogLevel.info,
+      ),
+    };
+    const agent = new Agent({
+      config,
+      dependencies: agentDependencies,
+      modules: agentModules,
+    });
+    agent.registerOutboundTransport(new HttpOutboundTransport());
+    agent.registerOutboundTransport(new WsOutboundTransport());
+    agent.registerInboundTransport(
+      new HttpInboundTransport({ port: agent_port }),
+    );
+    try {
+      await agent.initialize();
+      await createAndRegisterIndy(agent, seed, did, did_namespace);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+
+    return agent;
+  },
+  inject: [ConfigService, 'AGENT_MODULES'],
+};
+@Module({
+  providers: [agentFactory, ConfigService, agentModuleFactory],
+  exports: [AGENT_TOKEN],
+})
+export class AgentModule {
+  constructor(@Inject(AGENT_TOKEN) private agent: Agent) {}
+}

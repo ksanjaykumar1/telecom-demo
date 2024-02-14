30c448cc29478e3bb94e9dae7a4fea529285a2a5
Sanjay <ksanjayk@protonmail.com>
2024-02-14 19:27:41 +0530

    schema publishing and querying

---
+++ nest-agent/src/agent/agentUtils/functions/schema.ts
@@ -0,0 +1,45 @@
+import { Agent } from '@aries-framework/core';
+import { log, underscore } from '../../../utils';
+
+type CredentialSchema = {
+  attrNames?: string[];
+  issuerId: string;
+  name?: string;
+  version?: string;
+};
+
+export const createAndRegisterSchema = async (
+  issuer: Agent,
+  {
+    attrNames = ['name', 'date of birth', 'email', 'occupation'],
+    issuerId,
+    name = 'SSI Identity',
+    version = `1.0.${Math.floor(Math.random() * 1000)}`,
+  }: CredentialSchema,
+): Promise<string> => {
+  const schemaResult = await issuer.modules.anoncreds.registerSchema({
+    schema: {
+      attrNames,
+      issuerId,
+      name,
+      version,
+    },
+    options: {},
+  });
+
+  if (schemaResult.schemaState.state === 'failed') {
+    throw new Error(`Error creating schema`);
+  }
+  await log(
+    `Created ${underscore('schema')} with id '${underscore(
+      schemaResult.schemaState.schemaId!,
+    )}`,
+  );
+
+  return schemaResult.schemaState.schemaId!;
+};
+
+export const getSchemaById = async (agent: Agent, schemaId: string) => {
+  const schema = await agent.modules.anoncreds.getSchema(schemaId);
+  return schema;
+};

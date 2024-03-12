03610f4698e5069f2386432b8c6407a32f5f0f40
Sanjay <ksanjayk@protonmail.com>
2024-03-12 18:31:12 +0530

    production setup

--- telecom-application/server.js
+++ telecom-application/server.js
@@ -6,6 +6,9 @@ import 'express-async-errors';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 import authRouter from './routes/authRoutes.js';
 // import jobRouter from './routes/jobsRoutes.js';
@@ -17,6 +20,11 @@ import connectDB from './db/connect.js';
 
 const port = process.env.PORT || 5000;
 
+// we are doing this because we are using ES6 module and dirname is not accessible by default
+const __dirname = dirname(fileURLToPath(import.meta.url));
+// only when ready to deploy
+app.use(express.static(path.resolve(__dirname, './client/dist')));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
@@ -24,16 +32,20 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
 }
 
-app.post('', (req, res) => {
-  console.log(req.body);
-  res.json({ msg: 'Welcome' });
-});
+// app.post('', (req, res) => {
+//   console.log(req.body);
+//   res.json({ msg: 'Welcome' });
+// });
 
 app.get('/api/v1', (req, res) => {
   res.json({ msg: 'Welcome' });
 });
 
 app.use('/api/v1/auth', authRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
+});
 
 app.use(notFound);
 app.use(errorHandler);
